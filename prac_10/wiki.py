cc9bc1c1a6e97979053fae7ccfd9c4da93b4c64f
Zwe Sett Aung <gigozssa8@gmail.com>
2024-01-18 02:03:02 +0800

    modified

---
+++ prac_10/wiki.py
@@ -0,0 +1,18 @@
+import wikipedia
+
+def search():
+    keyword = input("Please input the search keyword: ")
+    while keyword != "":
+        try:
+            page_results = wikipedia.page(keyword)
+            print(page_results.title)
+            print(page_results.summary)  # Fix the typo here
+            print(page_results.url)
+        except wikipedia.exceptions.DisambiguationError as e:
+            print(e.options)
+        keyword = input("Please input the search keyword: ")
+    print("Thank you")
+
+search()
+
+
